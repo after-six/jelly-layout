3531751d5f694e45886b71de5abafc5828eda95d
jelly <chm073@gmail.com>
2020-04-19 22:39:53 +0900

    CrossAxisAlignment stretch Row

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         body: Container(
           color: Colors.yellow,
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Icon(Icons.star, size: 50),
               Icon(Icons.star, size: 200),
