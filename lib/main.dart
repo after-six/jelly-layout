4ea51455ccaad0f1fce1c8fc4110339e2e43591c
jelly <chm073@gmail.com>
2020-04-19 23:50:37 +0900

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -18,26 +18,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Jelly layout"),
         ),
-        body: Row(children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(color: Colors.red),
-            ),
-            flex: 3,
-          ),
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(color: Colors.green),
-            ),
-            flex: 2,
-          ),
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(color: Colors.yellow),
-            ),
-            flex: 1,
-          )
-        ]),
+        body: Card(child: const Text('Hello World!'), color: Colors.yellow)
+
       ),
     );
   }
