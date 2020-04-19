b94744d196b40c717df53f3e158bc07dc38f8f31
jelly <chm073@gmail.com>
2020-04-19 22:40:58 +0900

    MainAxisSize min Row

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Container(
           color: Colors.yellow,
-          child: Column(
+          child: Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               Icon(Icons.star, size: 50),
