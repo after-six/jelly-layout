ff3ccf9602206851fc3ba83e377898d7a3765efd
jelly <chm073@gmail.com>
2020-04-19 22:31:18 +0900

    MainAxisAlignment Row

---
+++ android/app/src/main/kotlin/kr/jellyms/jellylayout/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.jellyms.jellylayout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Jelly layout"),
+        ),
+        body: Container(
+          color: Colors.yellow,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Icon(Icons.star, size: 50),
+              Icon(Icons.star, size: 50),
+              Icon(Icons.star, size: 50),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
