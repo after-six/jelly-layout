e9c81f06b30ab3743ecdc7b0a7b7e714fbe16bd7
jelly <chm073@gmail.com>
2020-04-19 22:33:40 +0900

    MainAxisAlignment space between Row

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         body: Container(
           color: Colors.yellow,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Icon(Icons.star, size: 50),
               Icon(Icons.star, size: 50),
